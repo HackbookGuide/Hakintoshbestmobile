18c358795fd7a352ce48ce248b6f41f1ad74c00d
Hex2000 <hgamerinfinit@gmail.com>
2025-06-05 16:24:32 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.html",
-    "./js/**/*.js"
+    "./**/*.html",    // All HTML files in any subdirectory
+    "./js/**/*.js",   // All JS files in js directory and subdirectories
+    "./src/**/*.css"  // All CSS files in src directory and subdirectories
   ],
-  theme: { extend: {} },
-  plugins: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4A90E2',
+        secondary: '#f0f4f8'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
 }
